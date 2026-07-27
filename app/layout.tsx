e25a44ae47b79c9e91188f1f04e7e7e603ecef98
Carmin Vuong <carminvuong@gmail.com>
2026-07-27 12:41:14 -0400

    change title + favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "job_tracker",
+  title: "job tracker",
   description: "Personal internship application tracker",
 };
 
